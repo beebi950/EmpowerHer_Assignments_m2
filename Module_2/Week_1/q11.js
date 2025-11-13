fe56335d2133d3f590cf2a29e57c9fb049b7bb8f
beebi950 <beebibeebi@gmail.com>
2025-11-13 15:46:28 +0530

    step 1 done

--- Module_2/Week_1/q11.js
+++ Module_2/Week_1/q11.js
@@ -1,11 +1,27 @@
 let num = 20;
 
-function OddEven(num) {
-    if (num % 2 == 0) {
-        console.log("Even");
-    } else {
-        console.log("Odd");
+function Prime(num) {
+    for(let i=0;i<=num;i++){
+        let count=0;
+        if(num%i==0){
+            count ++
+        }
+    }
+
+    if(count==2){
+        return true
+
+    }else{
+        return false
     }
 }
 
-OddEven(num); 
+Prime(num); 
+
+//check prime 
+
+
+
+
+
+ 
