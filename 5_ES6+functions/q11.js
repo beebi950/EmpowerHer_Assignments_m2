e294c56bfb78e15ba94d94a8c008138a28849fd1
beebi-k <kalagotlabeebi95@gmail.com>
2025-11-21 15:58:49 +0530

    q11 of ES6 Assinment

---
+++ 5_ES6+functions/q11.js
@@ -0,0 +1,12 @@
+
+function multiply(a, b) {
+  return a * b;
+}
+
+const multiplyArrow = (a = 1, b = 1) => {
+  return a * b;
+};
+
+
+console.log(multiplyArrow(3, 4)); 
+console.log(multiplyArrow());     
