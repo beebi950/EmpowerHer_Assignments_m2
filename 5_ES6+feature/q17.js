7a5ca1843771b9e673c17e66b56db6b4fe188c50
beebi-k <kalagotlabeebi95@gmail.com>
2025-11-21 16:19:46 +0530

    ES6 q17 Assinment

---
+++ 5_ES6+feature/q17.js
@@ -0,0 +1,29 @@
+// a) Merge arrays using spread operator
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5];
+const mergedArray = [...arr1, ...arr2];
+console.log(mergedArray); 
+// [1, 2, 3, 4, 5]
+
+
+// b) Function using rest operator to sum numbers
+const sum = (...nums) => nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum(1, 2, 3, 4)); 
+// 10
+
+
+// c) Object destructuring
+const user = {
+  name: "Alice",
+  age: 22,
+  address: {
+    city: "Bangalore",
+    pin: 560001
+  }
+};
+
+const { name, address: { city, pin } } = user;
+
+console.log(name); 
+console.log(city); 
+console.log(pin);  
